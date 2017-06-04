a9ee027e1c3aa018fba0a0f9f6289ae26c2dbfc8
Brent <brent.hutchin@gmail.com>
2017-06-04 14:08:29 +1000

    added in neslogo and key control example

--- bin/launcher_2.py
+++ bin/launcher_2.py
@@ -17,20 +17,34 @@ class Example(QWidget):
     def initUI(self):      
 
         hbox = QHBoxLayout(self)
-        pixmap = QPixmap("../ui/elements/background_small.png")
+        background = QPixmap("../ui/elements/background_small.png")
+	nes = QPixmap("../ui/elements/nes.jpg")
+        bg = QLabel(self)
+	neslogo = QLabel(self)
+        bg.setPixmap(background)
+        neslogo.setPixmap(nes)
+	neslogo.raise_()
 
-        lbl = QLabel(self)
-        lbl.setPixmap(pixmap)
-
-        hbox.addWidget(lbl)
+        hbox.addWidget(bg)
+        hbox.addWidget(neslogo)
         self.setLayout(hbox)
 	#self.resize(1920, 1080)
         #self.move(10, 10)
-        #self.setWindowTitle('Dolos')
-	self.showFullScreen()
-        #self.show()        
+        self.setWindowTitle('Dolos')
+	#self.showFullScreen()
+        self.show()        
         
         
+    def control(self):
+	if event == QtCore.Qt.Key_Escape:
+            self.close()
+        if event == QtCore.Qt.Key_Left:
+            self.animFlag = False
+            self.nextImage()
+        if event == QtCore.Qt.Key_Right:
+            self.animFlag = True
+            self.nextImage()
+
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
