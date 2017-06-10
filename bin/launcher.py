298af92a918fbf499e41da67ddb487a2eff82388
Brent <brent.hutchin@gmail.com>
2017-06-10 18:39:30 +1000

    updated launcher to allow for grid layout

--- bin/launcher.py
+++ bin/launcher.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLayout, QHBoxLayout
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLayout, QGridLayout
+from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QPixmap
+import subprocess
 
 
 class dolos(QWidget):
@@ -17,20 +18,25 @@ class dolos(QWidget):
 		self.setStyleSheet("background-image: url(/home/atlas/dev/dolos/ui/elements/background.png); border: none")
 		self.showFullScreen()
 		self.setFocus
+		
 		nes = QLabel()
-		nes.setPixmap(QPixmap("/home/atlas/dev/dolos/ui/elements/nes.jpg"))
+		nesimage = QPixmap("/home/atlas/dev/dolos/ui/elements/nes.png")
+		nes.setPixmap(nesimage)
+		
 		snes = QLabel()
-		snes.setPixmap(QPixmap("/home/atlas/dev/dolos/ui/elements/snes.jpg"))
+		snes.setPixmap(QPixmap("/home/atlas/dev/dolos/ui/elements/snes.png"))
 		mame = QLabel()
 		mame.setPixmap(QPixmap("/home/atlas/dev/dolos/ui/elements/mame.png"))
 		indie = QLabel()
-		indie.setPixmap(QPixmap("/home/atlas/dev/dolos/ui/elements/indie.jpg"))
-		hbox = QHBoxLayout()
-		hbox.setSpacing(0)
-		hbox.addWidget(nes)
-		hbox.addWidget(snes)
-		hbox.addWidget(mame)
-		self.setLayout(hbox)
+		indie.setPixmap(QPixmap("/home/atlas/dev/dolos/ui/elements/indie.png"))
+		layout = QGridLayout()
+		#layout = QHBoxLayout()
+		#layout.setSpacing(200)
+		layout.addWidget(nes, 2, 1)
+		layout.addWidget(mame, 2, 2)
+		layout.addWidget(snes, 2, 3)
+		layout.addWidget(indie, 2, 4)
+		self.setLayout(layout)
 		#self.show()
 
 	def keyPressEvent(self, event):
@@ -43,7 +49,9 @@ class dolos(QWidget):
 		elif event.key() == Qt.Key_Right:
 			print('Right')
 		elif event.key() == Qt.Key_A:
-			print('Select')
+			args = ("/opt/pycharm-community-2017.1.3/bin/pycharm.sh")
+			subprocess.Popen(args)
+			print('launched'+args)
 
 
 if __name__ == '__main__':
